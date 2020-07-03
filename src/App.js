ebdf4cba54b711edecef751b59dd0f794ccd5f44
xiangqingyi <1352493573@qq.com>
2020-07-03 15:14:30 +0800

    add 初始化

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import '../public/style/jsmind.css';
+import '../public/js/jsmind.js';
+import '../public/js/jsmind.draggable.js';
+import '../public/js/jsmind.screenshot.js';
+import '../public/js/jsmind.shell.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      change
+    </div>
+  );
+}
+
+export default App;
